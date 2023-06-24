332aecffe8b0d10b2c2b14f32a252b9fc26d11f1
t.maleki <fa.maleki@azki.com>
2023-06-24 14:41:11 +0330

    First Comit

---
+++ src/test/java/com/softwaredesign/recruitservice/RecruitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwaredesign.recruitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
